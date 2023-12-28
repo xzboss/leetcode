949680e90de98bb80adff0cbdf0994e260803298
xzboss <942875315@qq.com>
2023-12-28 13:53:54 +0800

    feat: 🎸 最大公共前缀

--- easy/020-最后一个单词长度.js
+++ easy/020-最后一个单词长度.js
@@ -26,10 +26,10 @@
 var lengthOfLastWord = function(s) {
   let i = s.length - 1
   let length = 0
-  while(i > 0 && s[i] === ' '){
+  while(i >= 0 && s[i] === ' '){
     i--
   }
-  while(i > 0 && s[i] !== ' '){
+  while(i >= 0 && s[i] !== ' '){
     length++
     i--
   }
