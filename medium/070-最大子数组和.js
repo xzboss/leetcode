ea7d80a1da26c4ce106e3b4572f8c926bfdaf1f2
xuz <xuz@wanthings.com>
2024-09-03 10:28:50 +0800

    ✨ Feat: 最大子数组和

--- medium/070-最大子数组和.js
+++ medium/070-最大子数组和.js
@@ -1,7 +1,3 @@
-
-
-
-
 /**
 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
 
@@ -25,17 +21,51 @@
 输出：23
  */
 
-
-
-
-
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+// 暴力枚举法，超时
+// var maxSubArray = function (nums) {
+//   let start = 0;
+//   let end = 0;
+//   let max = nums[0];
+//   for (let i = 0; i < nums.length; i++) {
+//     let sum = 0;
+//     for (let j = i; j < nums.length; j++) {
+//       sum += nums[j];
+//       if (sum > max) {
+//         start = i;
+//         end = j;
+//         max = sum;
+//       }
+//     }
+//   }
+//   return max;
+// };
+// console.log(maxSubArray([-2,1]));
 
+// 动态规划
+var maxSubArray = function (nums) {
+  const dp = new Array(nums.length).fill(nums[0]);
+  let max = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    dp[i] = Math.max(nums[i] + dp[i - 1], nums[i]);
+    max = Math.max(dp[i], max);
+  }
+  return max;
 };
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
 
-
-//module.exports = 
+// Kadane :对dp数组的优化，空间复杂度为1
+// 其实这里有用的只是前面一个为终点的最大值
+var maxSubArray = function (nums) {
+  let max = nums[0];
+  let dp = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    dp = Math.max(dp + nums[i], nums[i]);
+    max = Math.max(dp, max);
+  }
+  return max;
+};
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
