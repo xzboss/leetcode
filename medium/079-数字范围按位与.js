70ad9603eb7540464b7f6f179891902da160a736
xuz <xuz@wanthings.com>
2024-09-10 10:49:26 +0800

    ✨ Feat: 数字范围按位与

--- medium/079-数字范围按位与.js
+++ medium/079-数字范围按位与.js
@@ -10,13 +10,33 @@
  * @param {number} right
  * @return {number}
  */
-// 1. 一个一个与
+// 1. 一个一个与 : 超时
 var rangeBitwiseAnd = function (left, right) {
-    let ans = 0;
-    for (let i = left; i <= right; i++) {
-      ans &= i;
-    }
-    return ans;
-  };
-  // @lc code=end
-  
+  let ans = left;
+  for (let i = left; i <= right; i++) {
+    ans &= i;
+  }
+  return ans;
+};
+
+// 2.由于数是连续的，所以他们的位与结果是他们的公共前缀
+// 采用右移方式去做
+var rangeBitwiseAnd = function (left, right) {
+  let shift = 0;
+  while (left < right) {
+    left >>= 1;
+    right >>= 1;
+    shift++;
+  }
+  return left << shift;
+};
+
+// 3. 任然是公共前缀，但是是倒序与
+var rangeBitwiseAnd = function (left, right) {
+  while (left < right) {
+    right = right & (right - 1);
+  }
+  return right;
+};
+console.log(rangeBitwiseAnd(1, 2));
+// @lc code=end 11110011 11110010
