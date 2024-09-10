53087d17030bd1c98136a68852fa8436b2eac826
xuz <xuz@wanthings.com>
2024-09-10 11:50:26 +0800

    ✨ Feat: 加一

--- easy/10-加一.js
+++ easy/10-加一.js
@@ -2,17 +2,32 @@
  * @param {number[]} digits
  * @return {number[]}
  */
+
+// 1. 从后向前，carry 记录是否进位
 var plusOne = function (digits) {
-  let length = digits.length - 1
-  while (digits[length] === 9) {
-    digits[length] = 0
-    length--
+  let carry = 1;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    digits[i] = (digits[i] + carry) % 10;
+    if (digits[i] !== 0) {
+      carry = 0;
+      break;
+    }
   }
-  if (length !== -1) {
-    digits[length]++
+  carry === 1 && digits.unshift(carry);
+  return digits;
+};
+// 2. 从后向前直接找连续的第一个9
+var plusOne = function (digits) {
+  let p = digits.length - 1;
+  while (digits[p] === 9) {
+    digits[p] = 0;
+    p--;
+  }
+  if (p === -1) {
+    digits.unshift(1);
   } else {
-    digits.unshift(1)
+    digits[p]++;
   }
-  return digits
-}
-console.log(plusOne([0, 9, 9]))
+  return digits;
+};
+console.log(plusOne([9]));
