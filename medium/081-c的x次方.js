2245ce26bf46b6e268e1273b44122516104ec589
xuz <xuz@wanthings.com>
2024-09-10 17:57:28 +0800

    ✨ Feat: 快速幂暂存

---
+++ medium/081-c的x次方.js
@@ -0,0 +1,45 @@
+/**
+输入：x = 2.00000, n = 10
+输出：1024.00000
+示例 2：
+
+输入：x = 2.10000, n = 3
+输出：9.26100
+示例 3：
+
+输入：x = 2.00000, n = -2
+输出：0.25000
+解释：2-2 = 1/22 = 1/4 = 0.25    
+ */
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+// 1 , 2147483647 超时
+var myPow = function (x, n) {
+  if (n == 0) return 1;
+  let temp = Math.abs(n);
+  let ans = 1;
+  while (temp > 0) {
+    ans *= x;
+    temp--;
+  }
+  return n > 0 ? ans : 1 / ans;
+};
+
+// 快速幂
+// 很简单一个道理 比如 2 ** 9 = (2 ** 8 * 1) * (2 ** 4 * 0) * (2 ** 2 * 0) * (2 ** 1 * 1)，9 的二进制 1001
+var myPow = function (x, n) {
+  if (n < 0) {
+    x - 1 / x;
+    n = -n;
+  }
+  while(n > 0){
+    const bit = n & 1
+     
+  }
+};
+console.log(myPow(1, 2147483647));
+
+//module.exports =
---
+++ test/m_081-c的x次方.test.js
@@ -0,0 +1,4 @@
+const fun = require('../medium/081-c的x次方.js');
+test('m_081-c的x次方', () => {
+	expect(fun()).toBe()
+})
