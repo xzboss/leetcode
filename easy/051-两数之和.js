3e2f4d8ba337f20fead501e33b6ed1e353b0d542
xuz <xuz@wanthings.com>
2024-09-25 10:07:06 +0800

    ✨ Feat: 两数之和

---
+++ easy/051-两数之和.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      return [map.get(nums[i]), i];
+    }
+    map.set(target - nums[i], i);
+  }
+};
+
+//module.exports =
---
+++ test/e_051-两数之和.test.js
@@ -0,0 +1,4 @@
+const fun = require('../easy/051-两数之和.js');
+test('e_051-两数之和', () => {
+	expect(fun()).toBe()
+})
