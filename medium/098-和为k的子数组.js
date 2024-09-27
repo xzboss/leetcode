2d9894a776ffc7b02f90c615a062210281fca1f8
xuz <xuz@wanthings.com>
2024-09-27 16:57:09 +0800

    ✨ Feat: 和为k的子数组

---
+++ medium/098-和为k的子数组.js
@@ -0,0 +1,41 @@
+/**
+给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
+
+子数组是数组中元素的连续非空序列。
+
+ 
+
+示例 1：
+
+输入：nums = [1,1,1], k = 2
+输出：2
+示例 2：
+
+输入：nums = [1,2,3], k = 3
+输出：2
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+// 前缀和 + hash表
+var subarraySum = function (nums, k) {
+  const map = { 0: 1 };
+  let preSum = 0;
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    preSum += nums[i];
+    count += map[preSum - k] || 0;
+    map[preSum] = map[preSum] ? map[preSum] + 1 : 1;
+  }
+  return count;
+};
+console.log(subarraySum([1, 3, 4, 4], 8));
+//module.exports =
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
---
+++ test/m_098-和为k的子数组.test.js
@@ -0,0 +1,4 @@
+const fun = require('../medium/098-和为k的子数组.js');
+test('m_098-和为k的子数组', () => {
+	expect(fun()).toBe()
+})
