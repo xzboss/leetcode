8580db53ff4e7ab285721619f1bb1a30f5ae1513
xuz <xuz@wanthings.com>
2024-10-08 15:55:44 +0800

    ✨ Feat: 翻转链表

---
+++ easy/054-反转链表.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let p = head;
+  let pre = null;
+  while (p) {
+    const temp = p.next;
+    p.next = pre;
+    pre = p;
+    p = temp;
+  }
+  return pre;
+};
+
+//module.exports =
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
---
+++ test/e_054-反转链表.test.js
@@ -0,0 +1,4 @@
+const fun = require('../easy/054-反转链表.js');
+test('e_054-反转链表', () => {
+	expect(fun()).toBe()
+})
